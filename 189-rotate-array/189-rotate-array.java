5eb08fff7ee84021391a6641d2172052d99767de
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-03-19 21:23:38 +0530

    Time: 2 ms (46.96%), Space: 64.4 MB (47.32%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,7 +1,9 @@
 class Solution {
 
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
+        k = k % nums.length;   //If k is larger than nums.length 
+        //For ex: if nums.length = 7 and number of rotation we have to do is 8
+//         then in 7th rotation it would be same as without rotaion i.e 8th rotation would be equivalent to 1 rotation therefore k%nums.length
         int[] tempArr = new int[k];
         int p = 0;
         for (int i = nums.length - k; i < nums.length; i++) {
