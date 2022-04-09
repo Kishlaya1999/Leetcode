516e2f1944517bbab65bb9b5b9bf1efc725e483d
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-04-09 20:58:54 +0530

    Time: 15 ms (66.51%), Space: 51.5 MB (10.37%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -15,18 +15,16 @@ class Solution {
             }
         }
         
-        // HashMap<Integer,Integer> map = new HashMap<>();
-        
-        // freq.forEach((k1,v) -> map.put(v,k1));
-        
-        // int n = map.size();
-        
+//      Creating the max priority queue according to the value(Frequency of the map)
+//      Priority Queue is of the type map entries i.e k->v pair   
         PriorityQueue <Map.Entry<Integer,Integer>> kFreq = new PriorityQueue <> ((a,b)->b.getValue() - a.getValue());
         
+//      Adding all the entries of frequency map to max priority queue
         kFreq.addAll(freq.entrySet());
         
         int[] ans = new int[k];
         
+//      Storing the top k elements (which are also the top frequency elements) in array   
         for(int i = 0; i < k; i++){
             ans[i] = kFreq.poll().getKey();
         }
