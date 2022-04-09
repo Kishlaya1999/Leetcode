d7af6dbad547bbdf3a82dd62dd050ad4d27d45a3
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-04-09 22:29:16 +0530

    Time: 22 ms (48.93%), Space: 53.2 MB (15.67%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,61 +1,28 @@
 class KthLargest {
-
     int k;
+//  Creating the min priority Queue(Min heap)   
     private PriorityQueue<Integer> min_pq = new PriorityQueue<>();
-    // ArrayList<Integer> arr;
-    
+
     public KthLargest(int k, int[] nums) {
-        this.k = k;
-        // this.nums = new nums[nums.length]; 
-        // arr = new ArrayList<Integer>();
-        for(int i = 0; i < nums.length; i++){
-            // this.nums[i] = nums[i];
-            // max_pq.add(nums[i]);
-            // arr.add(nums[i]);
+        this.k = k;         //Initializing the value of k
+//      Adding only k elements to the min hep and if the size increases by k then pop the last added eleemnt   
+        for (int i = 0; i < nums.length; i++) {
             min_pq.add(nums[i]);
-            if(min_pq.size()>k){
+            if (min_pq.size() > k) {
                 min_pq.poll();
             }
         }
+//      By doing this min heap will always contain k largest element and kth largest element will always on the top    
     }
-    
-    public int add(int val){
+
+    public int add(int val) {
         min_pq.add(val);
-        if(min_pq.size()>k){
+        if (min_pq.size() > k) {
             min_pq.poll();
         }
         return min_pq.peek();
     }
-    
-    // public int add(int val){
-    //     arr.add(val);
-    //     Collections.sort(arr);
-    //     return arr.get(arr.size()-k);
-    // }
-    
-    // public int add(int val) {
-    //     max_pq.add(val);
-    //     int ans = 0;
-    //     int [] arr = new int[k];
-    //     // ArrayList<Integer> arr = new ArrayList<>();
-    //     for(int i = 0; i < k-1; i++){
-    //         arr[i] = max_pq.poll();
-    //         // if(i==k){
-    //         //     ans = temp;
-    //         // }
-    //         // max_pq.add(temp);
-    //     }
-    //     for(int i =0 ; i< arr.length; i++){
-    //         max_pq.add(arr[i]);
-    //     }
-    //     // int ans = max_pq.peek();
-    //     // for(int i = 0; i < arr.size(); i++){
-    //     //     max_pq.add(arr.get(i));
-    //     // }
-    //     return arr[arr.length-1];
-    // }
 }
-
 /**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest obj = new KthLargest(k, nums);
