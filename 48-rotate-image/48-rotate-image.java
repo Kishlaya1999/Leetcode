a7de27d739dec4818ac5b112b7bd2cdbfb9784af
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-04-11 20:15:55 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (90.14%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -2,6 +2,7 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         
+        //Finding the transpose of a matirx
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(i < j){
@@ -10,11 +11,15 @@ class Solution {
             }
         }
         
+        // Reversing each row of the matrix
         for(int i = 0; i < n; i++){
             reverse(matrix[i]);
         }
+        
+        // The resultant matrix after treanspose and reversing each row will be rotated in clockwise direciton
     }
     
+//  Reverse function   
     public void reverse(int[] arr){
         int i = 0;
         int j = arr.length - 1;
@@ -30,6 +35,7 @@ class Solution {
         }
     }
     
+//  Swap function   
     public void swap(int[][] matrix, int i, int j){
         int temp = matrix[i][j];
         matrix[i][j] = matrix[j][i];
