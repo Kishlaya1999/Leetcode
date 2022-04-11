47b7f2d0ab82910e8a8e9376a1b4c94bdc9837f1
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-04-11 16:49:10 +0530

    Time: 5 ms (92.64%), Space: 42.6 MB (99.61%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -3,23 +3,22 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] arr = new int[m][n];
         k = k % (m * n);
 
-        if (k == 0) {
-            List<List<Integer>> ans = new ArrayList();
+//         if (k == 0) {
+//             List<List<Integer>> ans = new ArrayList();
 
-            for (int i = 0; i < m; i++) {
-                List<Integer> row = new ArrayList();
-                for (int j = 0; j < n; j++) {
-                    row.add(grid[i][j]);
-                }
-                ans.add(row);
-            }
-            return ans;
-        }
+//             for (int i = 0; i < m; i++) {
+//                 List<Integer> row = new ArrayList();
+//                 for (int j = 0; j < n; j++) {
+//                     row.add(grid[i][j]);
+//                 }
+//                 ans.add(row);
+//             }
+//             return ans;
+//         }
 
-        for (int l = 0; l < k; l++) {
+        while(k>0){
             int temp = grid[m - 1][n - 1];
             for (int i = m - 1; i >= 0; i--) {
                 for (int j = n - 1; j > 0; j--) {
@@ -31,10 +30,9 @@ class Solution {
                     grid[0][0] = temp;
                 }
             }
-            // k = k - 1;
+            k--;
         }
 
-        // List<List<Integer>> ans = new ArrayList<ArrayList<Integer>>();
         List<List<Integer>> ans = new ArrayList();
 
         for (int i = 0; i < m; i++) {
