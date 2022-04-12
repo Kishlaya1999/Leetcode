1b918f06177b419c1f47ea4c100ffb6218155326
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-04-12 20:12:04 +0530

    Time: 1 ms (55.24%), Space: 42.4 MB (49.68%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        
+        int[][] arr = new int[m][n];
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                int live = 0; 
+                if(i-1 != -1 && j-1 != -1 && board[i-1][j-1] == 1)   live++;
+                if(i-1 != -1 && board[i-1][j] == 1)    live++;
+                if(i-1 != -1 && j+1 != n && board[i-1][j+1] == 1)  live++;
+                if(j-1 != -1 && board[i][j-1] == 1) live++;
+                if(j+1 != n && board[i][j+1] == 1)  live++;
+                if(i+1 != m && j-1 != -1 && board[i+1][j-1] == 1) live++;
+                if(i+1 != m && board[i+1][j] == 1)  live++;
+                if(i+1 != m && j+1 != n && board[i+1][j+1] == 1)    live++;
+                
+                if(board[i][j] == 1 && live < 2)  arr[i][j] = 0;
+                else if(board[i][j] == 1 && live == 2 || live == 3) arr[i][j] = 1;
+                else if(board[i][j] == 1 && live > 3)   arr[i][j] = 0;
+                else if(board[i][j] == 0 && live == 3)  arr[i][j] = 1;
+            }
+        }
+        
+        for(int i = 0; i< board.length; i++){
+            for(int j = 0; j< board[0].length; j++){
+                board[i][j] = arr[i][j];
+            }
+        }
+            
+    }
+    
+}
