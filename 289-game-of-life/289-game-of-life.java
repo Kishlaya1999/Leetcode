02c8be56744ec78e532b5ab2a72e1b85ff5b8ac7
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-04-12 20:22:56 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (82.74%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -8,6 +8,7 @@ class Solution {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 int live = 0; 
+//              Traversing all the neighbours of current index
                 if(i-1 != -1 && j-1 != -1 && board[i-1][j-1] == 1)   live++;
                 if(i-1 != -1 && board[i-1][j] == 1)    live++;
                 if(i-1 != -1 && j+1 != n && board[i-1][j+1] == 1)  live++;
@@ -17,6 +18,7 @@ class Solution {
                 if(i+1 != m && board[i+1][j] == 1)  live++;
                 if(i+1 != m && j+1 != n && board[i+1][j+1] == 1)    live++;
                 
+//              assigning the value of current index accoring to the live count in other array  
                 if(board[i][j] == 1 && live < 2)  arr[i][j] = 0;
                 else if(board[i][j] == 1 && live == 2 || live == 3) arr[i][j] = 1;
                 else if(board[i][j] == 1 && live > 3)   arr[i][j] = 0;
@@ -24,6 +26,7 @@ class Solution {
             }
         }
         
+//      Copying the value of final array in original array    
         for(int i = 0; i< board.length; i++){
             for(int j = 0; j< board[0].length; j++){
                 board[i][j] = arr[i][j];
