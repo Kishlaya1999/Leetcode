29ecd3635619baf19be25deb2515707d11145249
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2022-04-26 00:50:17 +0530

    Time: 6 ms (61.79%), Space: 42.8 MB (58.43%) - LeetHub

--- 284-peeking-iterator/284-peeking-iterator.java
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -3,12 +3,17 @@
 
 class PeekingIterator implements Iterator<Integer> {
     
+    // Declaring the iterator and 
     Iterator<Integer> iter = null;
+    // a next object of integer type
     Integer next = null;
     
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
+        
+        // initiallizing the iterator
 	    iter = iterator;
+        // initializing the next if next exist
         if(iter.hasNext()){
             next = iter.next();
         }
@@ -23,17 +28,24 @@ class PeekingIterator implements Iterator<Integer> {
 	// Override them if needed.
 	@Override
 	public Integer next() {
+        // Assigning the current variable to ans for returning it
 	    int ans = next;
+        
+        // Moving the next variable if next exist else assiging it as null
         if(iter.hasNext()){
             next = iter.next();
         }else{
             next = null;
         }
+        
+        // Returning the variable which was assigned earlier
         return ans;
 	}
 	
 	@Override
 	public boolean hasNext() {
+        
+        // If current and next variable exist then returning true else false 
 	    return next != null || iter.hasNext();
 	}
 }
