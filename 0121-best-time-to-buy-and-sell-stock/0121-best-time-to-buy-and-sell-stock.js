7229e9141089650002534b5fa8438e814ff3f2ad
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2026-01-03 13:10:03 +0530

    Time: 0 ms (100%), Space: 65.5 MB (62.42%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,11 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let minVal = +Infinity, maxProfit = 0;
+
+  let minVal = +Infinity,   // variable for tracking the minValue of stock
+     maxProfit = 0;         // variable for tracking the maxProfit
     for (let i = 0 ; i< prices.length; i++) {
+      // Checking the current price of the stock if it is lesser than the price up until now then upating it with new minimum value
         if (prices[i] < minVal) {
           minVal = prices[i];
         }
+        // Calculating the profit for the current index and comparing it with the maxProfit which we have calculated up until now. If current profit is more than the previous profit value then upating the maxProfit variable
         if ((prices[i] - minVal) > maxProfit) {
           maxProfit = prices[i] - minVal;
         }
